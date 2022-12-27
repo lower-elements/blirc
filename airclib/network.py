1752c5963912457e742bf1cd926fb9c0524e3845
Michael Connor Buchan <mikey@blindcomputing.org>
2022-12-27 19:21:26 +0000

    Correctly use NETWORK isupport parameter for network naming

--- airclib/network.py
+++ airclib/network.py
@@ -2,21 +2,21 @@ import miniirc
 
 class Network:
     def __init__(self, name, cfg):
-        self.name = name
+        self._name = name
         creds = (cfg.get("nick"), cfg.get("password")) if "password" in cfg else None
         self.irc = miniirc.IRC(cfg.get("host"), cfg.getint("port"), cfg.get("nick"), ident=cfg.get("ident"), realname=cfg.get("realname"), ns_identity=creds)
         self.buffers = {}
 
     @property
-    def server_name(self):
+    def name(self):
         try:
             return self.irc.isupport["NETWORK"]
         except KeyError:
-            return self.irc.ip
+            return self._name
 
     def __repr__(self):
         match self.irc.connected:
             case True: s = ""
             case False: s = "[Connecting] "
             case _: s = "[Disconnected] "
-        return s + f"{self.name} ({self.server_name})"
+        return s + f"{self.name} ({self.irc.ip})"
