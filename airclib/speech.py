4fd9d09d68171105380c2fcc294017d80b7302a6
Michael Connor Buchan <mikey@blindcomputing.org>
2022-12-28 16:26:18 +0000

    Fix spam messages crashing speech-dispatcher
    Apparently sending too many messages at once to speechd isn't good, so
    we limit it to 1 message every 0.1 seconds.

--- airclib/speech.py
+++ airclib/speech.py
@@ -4,25 +4,30 @@ if platform.system() == "Windows":
     from cytolk import tolk
     tolk.try_sapi(True)
     tolk.load("__compiled__" not in globals())
-    print ("Windows speech deps loaded.")
 
 elif platform.system() == "Darwin":
     from . import NSSS
     speaker = NSSS.NSSS ()
-    print ("Darwin / XNU speech deps loaded.")
 
 elif platform.system() == "Linux":
     import speechd
-    linux_speaker = speechd.Speaker("Loading...")
+    from time import perf_counter
+    linux_speaker = speechd.Speaker("airc")
+    linux_speaker.set_priority(speechd.Priority.TEXT)
+    last = perf_counter()
 
 def speak (text, interupt = True):
     if platform.system() == "Windows":
         tolk.speak(text, interupt)
 
     elif platform.system() == "Linux":
-        if interupt:
-            linux_speaker.cancel()
-            linux_speaker.speak(text)
+        global last
+        elapsed = perf_counter() - last
+        if elapsed > 0.1:
+            if interupt:
+                linux_speaker.cancel()
+                linux_speaker.speak(text)
+                last = perf_counter()
 
     elif platform.system() == "Darwin":
         speaker.set ("rate", 300)
