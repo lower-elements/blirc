63b9b3a5bf7509985e07dc1fdd7d873d0eaa97f1
Michael Connor Buchan <mikey@blindcomputing.org>
2023-01-04 14:19:03 +0000

    feet: Better IME support using pygame.key.{start,stop}_text_input()
    Also fixes a long-standing bug where we had to detect the / key being
    pressed differently to avoid a leading / being inserted into every
    message.

--- blirc/__main__.py
+++ blirc/__main__.py
@@ -8,6 +8,8 @@ def main():
     pygame.display.init()
     pygame.display.set_caption("{} v{}.{}.{}".format(consts.TITLE, consts.VERSION[0], consts.VERSION[1], consts.VERSION[2]))
     screen = pygame.display.set_mode((800, 600))
+    pygame.key.set_text_input_rect((0, 0, 800, 600))
+    pygame.key.stop_text_input()
     ui = UI(screen)
     ui.main_loop()
 
